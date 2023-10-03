6c1bc27241cbb65047385258ffbf7e20e9507c3c
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2023-10-02 21:17:43 -0500

    Recipe-10/2

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+
+ // use Recipe model from 'recipe.model.ts'
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe-1', 'This is simpy a test description-1', 'https://sallysbakingaddiction.com/wp-content/uploads/2014/05/sugar-cookie-fruit-pizza.jpg' ),
+    new Recipe('A Test Recipe-2', 'This is simpy a test description-2', 'https://sallysbakingaddiction.com/wp-content/uploads/2014/05/sugar-cookie-fruit-pizza.jpg' )
+  ]; 
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
