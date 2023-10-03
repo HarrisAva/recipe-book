5d1d6ca2288eaad5da12f944cdc949525a1be37c
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2023-10-02 22:59:32 -0500

    recipe 10/2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { Ingredient } from './shared/ingredient.model';
 import { Recipe } from './recipes/recipe.model';
 
@@ -20,8 +18,7 @@ import { Recipe } from './recipes/recipe.model';
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
+
 
   ],
   imports: [
