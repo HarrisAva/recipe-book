0c82dea0121e9b01727ee55099901b0f53af15c5
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2023-10-30 22:21:50 -0500

    completed resolver and data storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,25 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 
-  
+
 const appRoutes: Routes = [
-    // pathMatch to only redirect if the full path is empty 
+    // pathMatch to only redirect if the full path is empty
     // (overide the default of prefix and empty path)
-    {path: '', redirectTo: '/recipes', pathMatch:'full'}, 
+    {path: '', redirectTo: '/recipes', pathMatch:'full'},
     {path: 'recipes', component: RecipesComponent, children: [
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailComponent},
-      {path: ':id/edit', component: RecipeEditComponent}
+      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
     ] },
     {path: 'shopping-list', component: ShoppingListComponent},
   ];
 
   @NgModule({
     // pass our appRoutes to configure/add to Angular/router
-    imports: [RouterModule.forRoot(appRoutes)], 
+    imports: [RouterModule.forRoot(appRoutes)],
 
     // export configured router in this extra module to main app.module
     exports: [RouterModule]
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,33 +5,42 @@ import { ShoppingListService } from "../shopping-list/shipping-list.service";
 import { Subject } from "rxjs";
 
 // use @Injectable to access shopping list service (inject service to service)
-@Injectable() 
+@Injectable()
 export class RecipeService {
 
   // when editing or adding a new recipe:
   recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Pizza',
-         'Pepperoni pizza',
-          'https://image.shutterstock.com/image-photo/supreme-pizza-lifted-slice-1-260nw-84904912.jpg',
-          [
-            new Ingredient ('Cheese', 1),
-            new Ingredient ('Pepperoni', 1),
-            new Ingredient ('Tomato', 20)
-          ] ),
-        new Recipe('Burger', 
-        'Beef burger', 
-        'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
-        [
-          new Ingredient ('Buns', 2),
-          new Ingredient ('Beef', 1),
-          new Ingredient ('Onion', 1)
-        ] )
-      ]; 
+    // private recipes: Recipe[] = [
+    //     new Recipe('Pizza',
+    //      'Pepperoni pizza',
+    //       'https://image.shutterstock.com/image-photo/supreme-pizza-lifted-slice-1-260nw-84904912.jpg',
+    //       [
+    //         new Ingredient ('Cheese', 1),
+    //         new Ingredient ('Pepperoni', 1),
+    //         new Ingredient ('Tomato', 20)
+    //       ] ),
+    //     new Recipe('Burger',
+    //     'Beef burger',
+    //     'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+    //     [
+    //       new Ingredient ('Buns', 2),
+    //       new Ingredient ('Beef', 1),
+    //       new Ingredient ('Onion', 1)
+    //     ] )
+    //   ];
+
+      // initialize recipes when loading app (no recipe)
+      private recipes: Recipe[] = [];
 
       //inject shopping list service
       constructor(private slService: ShoppingListService) {}
+
+      // fetch recipes from database, then overwrite recipe array here
+      setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+      }
 
       // to get a copy of list of recipes from other components
       getRecipes() {
